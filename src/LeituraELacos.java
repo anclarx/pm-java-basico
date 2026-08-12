a1b841062bb14f375c28730eaf12e81064c90bac
Ana <anaclarafreitas007@gmail.com>
2026-08-12 18:15:30 -0300

    ajuste codigo leitura

--- src/LeituraELacos.java
+++ src/LeituraELacos.java
@@ -22,87 +22,96 @@
  * SOFTWARE.
  */
 
-
 /** Java básico: E/S, repetição e decisão, vetores */
-   
-    /**
-     * Recebe uma mensagem para exibir e pede a leitura de um número inteiro, retornando-o. 
-     * Código sem robustez para valores não inteiros.
-     * @param mensagem Mensagem a ser exibida ao usuário
-     * @return Número inteiro lido a partir do teclado
-     */
-    int lerNumero(String mensagem){
-        int valor;
-        valor = Integer.parseInt( IO.readln("\t"+mensagem+": "));
-        return valor;
-    }
 
-    /**
-     * Preenche um vetor a partir da leitura de números inteiros
-     * @param vetor Vetor a ser preenchido
-     */
-     void preencherVetor(int[] vetor){
-        for (int i = 0; i < vetor.length; i++) {
-            vetor[i] = lerNumero("Digite um valor inteiro");
+/**
+ * Recebe uma mensagem para exibir e pede a leitura de um número inteiro,
+ * retornando-o.
+ * Código sem robustez para valores não inteiros.
+ * 
+ * @param mensagem Mensagem a ser exibida ao usuário
+ * @return Número inteiro lido a partir do teclado
+ */
+int lerNumero(String mensagem) {
+    int valor;
+    valor = Integer.parseInt(IO.readln(mensagem + ": "));
+    return valor;
+}
+
+/**
+ * Preenche um vetor a partir da leitura de números inteiros
+ * 
+ * @param vetor Vetor a ser preenchido
+ */
+void preencherVetor(int[] vetor) {
+    for (int i = 0; i < vetor.length; i++) {
+        vetor[i] = lerNumero("Digite um valor inteiro");
+    }
+}
+
+/**
+ * Recebe um vetor de inteiros como parâmetro e retorna a quantidade de números
+ * pares deste vetor.
+ * 
+ * @param vetor O vetor a ser analisado.
+ * @return int com a quantidade de pares do vetor (>=0)
+ */
+int contaImpares(int[] vetor) {
+    int contadorImpar = 0;
+    for (int i = 0; i < vetor.length; i++) {
+        contadorImpar += (vetor[i] % 2);
+    }
+    return contadorImpar;
+}
+
+int somaVetor(int[] vetor) {
+    int soma = 0;
+    for (int i = 0; i < vetor.length; i++) {
+        soma = soma + vetor[i];
+    }
+    return soma;
+}
+
+int maioresQue(double valor, int[] vetor) {
+    int contador = 0;
+    for (int i = 0; i < vetor.length; i++) {
+        if (vetor[i] > valor) {
+            contador = contador + 1;
         }
     }
+    return contador;
+}
 
-    /**
-     * Recebe um vetor de inteiros como parâmetro e retorna a quantidade de números pares deste vetor.
-     * @param vetor O vetor a ser analisado.
-     * @return int com a quantidade de pares do vetor (>=0)
-     */
-    int contaImpares(int[] vetor){
-        int contadorImpar=0;
-        for (int i = 0; i < vetor.length; i++) {
-            contadorImpar += (vetor[i]%2);
-        }
-        return contadorImpar;
-    }
+void exibirResultados(int quantImpares, int quantPares, int soma, double media, int maioresQueAMedia) {
 
-    int somaVetor(int [] vetor){
-        int soma = 0;
-        for (int i = 0; i < vetor.length; i++) {
-            soma = soma + vetor[i];
-        }
-        return soma;
-    }
+    IO.println("RESULTADOS:");
 
-    int maioresQue(double valor, int[] vetor){
-        int contador = 0;
-        for (int i = 0; i < vetor.length; i++) {
-            if(vetor[i] > valor){
-                contador = contador+1;
-            }
-        }
-        return contador;
-    }
+    String imparesPares = String.format("O vetor tem %d números ímpares e %d pares.", quantImpares, quantPares);
+    String somaMedia = String.format("A soma dos valores é de %d e sua média é de %f", soma, media);
+    String maioresMedia = String.format("%d valores são maiores que a média", maioresQueAMedia);
 
-    void main(){
-        
-        int[] vetor;
-        int tamanho = lerNumero("Quantidade de inteiros para ler");
-        int quantImpares;
-        int soma;
-        double media;
-        int maioresQueAMedia;
+    IO.println(imparesPares);
+    IO.println(somaMedia);
+    IO.println(maioresMedia);
+}
 
-        vetor = new int[tamanho];
-        preencherVetor(vetor);
-        quantImpares = contaImpares(vetor);
-        soma = somaVetor(vetor);
-        media = soma / tamanho;
-        maioresQueAMedia = maioresQue(media, vetor);
+void main() {
 
-        IO.println("RESULTADOS:");
-        IO.println("============");
-        IO.println("O vetor tem "+quantImpares+" números ímpares e "+(tamanho-quantImpares)+" pares.");
-        IO.println(
-                String.format("A soma dos valores é de %d e sua média é de %f", soma, media)
-                );
-        IO.println(
-                String.format("%d valores são maiores que a média", maioresQueAMedia)
-                );
-        
-    }
+    int[] vetor;
+    int tamanho = lerNumero("Quantidade de inteiros para ler");
+    int quantImpares;
+    int quantPares;
+    int soma;
+    double media;
+    int maioresQueAMedia;
 
+    vetor = new int[tamanho];
+    preencherVetor(vetor);
+    quantImpares = contaImpares(vetor);
+    quantPares = tamanho - quantImpares;
+    soma = somaVetor(vetor);
+    media = (double) soma / tamanho;
+    maioresQueAMedia = maioresQue(media, vetor);
+
+    exibirResultados(quantImpares, quantPares, soma, media, maioresQueAMedia);
+}
